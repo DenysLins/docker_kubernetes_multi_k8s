793f7a5fc958dbc8e12c06bc378caa635a79a3f4
Denys Lins <denys.lins@hp.com>
2020-04-19 20:51:16 -0300

    Fix react test and add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-echo 'Deployed'
+echo 'Deploy'
+
+# docker build -t denyslins/udemy-react-multi-client -f ./client/Dockerfile.dev ./client
+# docker build -t denyslins/udemy-react-multi-server -f ./server/Dockerfile.dev ./server
+# docker build -t denyslins/udemy-react-multi-worker -f ./worker/Dockerfile.dev ./worker
+
+# docker push denyslins/udemy-react-multi-client
+# docker push denyslins/udemy-react-multi-server
+# docker push denyslins/udemy-react-multi-worker
+
+# kubectl apply -f k8s
